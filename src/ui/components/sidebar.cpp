544efa81fcabd62f026e04157b21c14b38132336
UltimatePlayer97 <gogole.help134@gmail.com>
2026-06-05 10:32:23 +1000

    fix: fix documents and downloads folder redirecting to root folder

--- src/ui/components/sidebar.cpp
+++ src/ui/components/sidebar.cpp
@@ -4,15 +4,18 @@
 using namespace std;
 using namespace ftxui;
 
-SidebarComponent::SidebarComponent(const vector<filesystem::path>& system_paths,
-                                     function<void(const string&)> on_place_selected) {
+SidebarComponent::SidebarComponent(const vector<filesystem::path>& system_paths, function<void(const string&)> on_place_selected) {
+    targets_ = system_paths;
+
+    if (targets_.size() >= 4) {
+        targets_.insert(targets_.begin() + 1, filesystem::path("/"));
+    }
+
     MenuOption option;
     option.on_enter = [this, on_place_selected]() {
         if (selected_index_ >= 0 && selected_index_ < static_cast<int>(places_.size())) {
-            string target_path = "/";
-            if (places_[selected_index_] == "Home") target_path = "/home";
-
-            on_place_selected(target_path);
+            string actual_target_path = targets_[selected_index_].string();
+            on_place_selected(actual_target_path);
         }
     };
 
